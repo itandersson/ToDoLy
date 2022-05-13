eddc00b4f87a7a9491a8cca20523f24b3af160c6
Joakim Andersson <joakim@ktv-sjobo.se>
2022-05-13 10:08:34 +0200

    Add a while loop to funktion start

--- ConsoleView.cs
+++ ConsoleView.cs
@@ -9,7 +9,41 @@ namespace ToDoLy
 
         internal void start()
         {
-            Console.WriteLine("Startar programmet!");
+            Boolean run = true;
+            int x = list.Count;
+            int y = 0;
+
+            string welcome = "Welcome to ToDoLy\n" +
+                "You have " + x + " tasks todo and " + y + " tasks are done!\n\n" +
+                "(1) Show Task List (by date or project)\n" +
+                "(2) Add New Task\n" +
+                "(3) Edit Task (update, mark as done, remove)\n" +
+                "(4) Save and Quit\n";
+
+            while (run)
+            {
+                Console.WriteLine(welcome);
+                Console.Write("Pick an option: ");
+                int value = int.Parse(Console.ReadLine());
+
+                switch (value)
+                {
+                    case 1:
+                        //showTaskList(list);
+                        continue;
+                    case 2:
+                        //Task item = addNewTask();
+                        //list.Add(item);
+                        Console.WriteLine('\t' + "Thank you, a new task was successfully added." + '\n');
+                        continue;
+                    case 3:
+                        //editTask(list);
+                        continue;
+                    case 4:
+                        run = false;
+                        break;
+                }
+            }
         }
 
         public void AddNewTask()
